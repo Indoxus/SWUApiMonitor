d3b224158d7fa4a2cbee6a6b2c7c747feff9ae2a
Indoxus <paulwallerxy@gmail.de>
2025-07-29 12:37:46 +0200

    oo, only every ten seconds update, cut-off

--- monitor.py
+++ monitor.py
@@ -2,6 +2,7 @@ import requests
 import json
 import time
 import datetime
+import arrow
 import sys
 
 n = 5
@@ -38,52 +39,79 @@ def format_time(time):
 		out += str(seconds)
 	return out
 
+class Monitor:
+	timestamp = 0
+	station_name = ""
+	departures = []
+	stationid = 1111
+	last_update = 0
+	stopamount  = 5
+	truestopamount = 0
+	is_there_data = False
+	def __init__(self, stationid,stopamount=5):
+		self.stationid = stationid
+		self.stopamount = stopamount
+		self.update(force=True)
+	
+	def update(self, force = False):
+		if not force:
+			if time.time() - self.last_update  < 10:
+				return
+		try: 
+			r = requests.get('https://api.swu.de/mobility/v1/stop/passage/Departures?StopNumber='+str(self.stationid)+'&Limit='+str(self.stopamount))
+			self.last_update = time.time()
+		except: 
+			return
+		jsun = r.json()
+		try:
+			self.station_name = jsun["StopPassage"]["StopCode"]
+			data = jsun["StopPassage"]["DepartureData"]
+		except:
+			return
+		formated  = []
+		for i in range(n):
+			try:
+				temp = dict(data[i])
+				formated.append([temp["DepartureDirectionText"],temp["DepartureCountdown"]])
+			except:
+				self.truestopamount = i
+				break
+			if i == n-1:
+				self.truestopamount = i
+		self.departures = formated[:]
+		self.is_there_data = True
+
+	def print_data(self):
+		timepassed = int(time.time()-self.last_update)
+		if not self.is_there_data:
+			print("Abfahrten")
+			print("")
+			print("")
+			print("no data")
+			print("")
+			print("")
+			return
+		print("Abfahrten " + self.station_name)
+		print('{:%Y-%m-%d %H:%M:%S}'.format(arrow.get(self.last_update+2*60*60).datetime),"+",int(time.time()-self.last_update))
+		print("timetimetime")
+		if self.truestopamount == 0:
+			print("")
+			print("no data")
+			print("")
+			print("")
+			return
+		for f in self.departures:
+			line = format_time(f[1]-timepassed) + " " + f[0]
+			print(line[:24])
+		for i in range(self.stopamount-self.truestopamount):
+			print("")
+
+	
+mon = Monitor(1255)
 
 while True:
+	mon.update()
+	mon.print_data()
 	time.sleep(1)
-	try:
-		r = requests.get('https://api.swu.de/mobility/v1/stop/passage/Departures?StopNumber='+str(stop)+'&Limit='+str(n))
-	except:
-		print("Abfahrten")
-		print("")
-		print("")
-		print("connection refused")
-		print("")
-		print("")
-		continue
-	jsun = r.json()
-	blank=0
-	#print(r.text)
-	try:
-		data = jsun["StopPassage"]["DepartureData"]
-		#print(data)
-	except:
-		print("Abfahrten")
-		print("")
-		print("")
-		print("no data")
-		print("")
-		print("")
-		continue
-	formated = []
-	for i in range(n):
-		try:
-			temp = dict(data[i])
-			#print(temp)
-			formated.append([temp["DepartureDirectionText"],temp["DepartureCountdown"]])
-		except:
-			blank = n-1-i
-			break
-	
-	print("Abfahrten")
-	print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
-	if blank == n-1:
-		print("")
-		print("no data")
-		print("")
-		print("")
 
-	for f in formated:
-		print(format_time(f[1]),f[0])
-	for b in range(blank):
-		print("")
+#	print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
--- test.py
+++ test.py
@@ -4,3 +4,8 @@ import requests
 contents = requests.get("https://api.swu.de/mobility/v1/stop/passage/Departures?StopNumber=1255&Limit=2")
 print(contents.content)
 #{"StopNumber":1255,"StopCode":"EBST","StopName":"Eselsbergsteige","StopPointNumber":2,"StopPointCode":125502,"PlatformName":"A","StopPointName":"Eselsbergsteige Steig A"},
+
+array = [1,2,3,4,5,6]
+array2 = array[:]
+print(id(array),id(array2))
+print(len("18:22 Wissenschaftsstadt"))
